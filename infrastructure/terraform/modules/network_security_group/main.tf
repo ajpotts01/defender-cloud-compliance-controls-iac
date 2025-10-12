df34ac70e10054c8738821f76d4f69a4fb885af3
Alexander Potts <ajpotts01@gmail.com>
2025-10-12 22:04:05 +1100

    fix(tf): rdp rule

--- infrastructure/terraform/modules/network_security_group/main.tf
+++ infrastructure/terraform/modules/network_security_group/main.tf
@@ -7,11 +7,12 @@ resource "azurerm_network_security_group" "project_nsg" {
 
   # Note: public ingress security checks ignored at this time
   # The Microsoft lab is prescribing these being open.
+  #tfsec:ignore:azure-network-no-public-ingress  
   security_rule {
     name                                       = "allow-web-all"
     access                                     = "Allow"
     direction                                  = "Inbound"
-    source_address_prefix                      = "*" #tfsec:ignore:azure-network-no-public-ingress
+    source_address_prefix                      = "*"
     source_port_range                          = "*"
     destination_application_security_group_ids = [var.asgs["web_security_group"]]
     destination_port_ranges                    = ["80", "443"]
@@ -19,11 +20,12 @@ resource "azurerm_network_security_group" "project_nsg" {
     priority                                   = 100
   }
 
+  #tfsec:ignore:azure-network-no-public-ingress:tfsec:ignore:azure-network-disable-rdp-from-internet
   security_rule {
     name                                       = "allow-rdp-all"
     access                                     = "Allow"
     direction                                  = "Inbound"
-    source_address_prefix                      = "*" #tfsec:ignore:azure-network-no-public-ingress
+    source_address_prefix                      = "*" 
     source_port_range                          = "*"
     destination_application_security_group_ids = [var.asgs["mgmt_security_group"]]
     destination_port_range                     = "3389"
