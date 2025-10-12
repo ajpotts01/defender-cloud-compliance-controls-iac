a0d3eec746f8d267d07424b7d68e61725adddf33
Alexander Potts <ajpotts01@gmail.com>
2025-10-12 21:59:52 +1100

    fix(tf): tfsec ignores

--- infrastructure/terraform/modules/network_security_group/main.tf
+++ infrastructure/terraform/modules/network_security_group/main.tf
@@ -5,11 +5,13 @@ resource "azurerm_network_security_group" "project_nsg" {
   location            = var.region
   resource_group_name = var.resource_group
 
+  # Note: public ingress security checks ignored at this time
+  # The Microsoft lab is prescribing these being open.
   security_rule {
     name                                       = "allow-web-all"
     access                                     = "Allow"
     direction                                  = "Inbound"
-    source_address_prefix                      = "*"
+    source_address_prefix                      = "*" #tfsec:ignore:azure-network-no-public-ingress
     source_port_range                          = "*"
     destination_application_security_group_ids = [var.asgs["web_security_group"]]
     destination_port_ranges                    = ["80", "443"]
@@ -21,7 +23,7 @@ resource "azurerm_network_security_group" "project_nsg" {
     name                                       = "allow-rdp-all"
     access                                     = "Allow"
     direction                                  = "Inbound"
-    source_address_prefix                      = "*"
+    source_address_prefix                      = "*" #tfsec:ignore:azure-network-no-public-ingress
     source_port_range                          = "*"
     destination_application_security_group_ids = [var.asgs["mgmt_security_group"]]
     destination_port_range                     = "3389"
